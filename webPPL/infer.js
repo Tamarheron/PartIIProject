1fa505b76e712005b9ff9359ca49bcc78729746e
Elizabeth Barnes <beth.m.barnes@gmail.com>
2017-11-18 13:24:15 +0000

    human and robot regret

--- webPPL/infer.js
+++ webPPL/infer.js
@@ -1,13 +1,22 @@
+var max = function(a,b){
+  return Math.max(a,b);
+};
+
+
 var actions = ['A', 'B'];
-var startStates = ['start1','start2']
+var startStates = ['start1','start2'];
+var nextStates = ['A', 'B'];
 
 
 var transition = function(state, action) {
-  var nextStates = ['A', 'B'];
   var nextProbs = (action === 'A') ? [1,0] : [0,1];
   return categorical(nextProbs, nextStates);
 };
 
+//distributions of true parameters and robot's belief
+var TRUEBETA1 = Categorical({ps:[1,1,1], vs:[1,2,3]});
+var TRUEBETA2 = Categorical({ps:[1,1,1], vs:[1,2,3]});
+var TRUED = Categorical({ps:[1,1], vs:[-1,1]});
 
 var expectedUtility = function(state, action, utility) {
                 return utility(transition(state, action));
@@ -19,11 +28,17 @@ var softMaxAgent = function(state, beta, utility) {
         model() {
           var action = uniformDraw(actions);
           var eu = expectedUtility(state, action, utility);
-          print("action, state, beta, eu =");
-          print(action);
-          print(state);
-          print(beta);
-          print(eu);
+          var debugPrint = false;
+          if (debugPrint){
+            print("action, state, beta, eu =");
+            print(action);
+            print(state);
+            print(beta);
+            print(eu);
+            print("factor");
+            print(eu/beta);
+          }
+          
           factor(eu/beta);
           
         return action;
@@ -34,21 +49,20 @@ var softMaxAgent = function(state, beta, utility) {
 
 var makeTrajectory = function(getBeta, utility, length) {
   var step = function(){
-        var state = uniformDraw(startStates);
-        var action = sample(softMaxAgent(state, getBeta(state), utility));
-        return {state, action};
+    var state = uniformDraw(startStates);
+    var action = sample(softMaxAgent(state, getBeta(state), utility));
+    return [state, action];
   };
   var res = step()
-  return length==0 ? [] : [res].concat(makeTrajectory(getBeta, utility, length-1));
+  return length==1 ? [res] : [res].concat(makeTrajectory(getBeta, utility, length-1));
 };
 
-var observedTrajectory = [['start2','A'], ['start2','A'], ['start2','A']];
 
 var posterior = dp.cache(function(observedTrajectory){
   return Infer({model() {
   //define priors
-  var beta1 = sample(RandomInteger({n:8}))+1;
-  var beta2 = sample(RandomInteger({n:8}))+1;
+  var beta1 = sample(TRUEBETA1);
+  var beta2 = sample(TRUEBETA2);
   
   var getBeta = function(state){
     var table = {
@@ -58,7 +72,7 @@ var posterior = dp.cache(function(observedTrajectory){
     return table[state];
   };
   
-  var d = sample(RandomInteger({n:9}))-4
+  var d = sample(TRUED);
   var utilA = 0;
   var utilB = utilA + d;
   
@@ -79,16 +93,15 @@ var posterior = dp.cache(function(observedTrajectory){
       observe(softMaxAgent(state, beta, utility), action);
     },
     observedTrajectory);
-
-  //var choice = d<0 ? 'A' : 'B' //choose the action with higher utility
-  return {d};
+    return {d};
   }});
 });
 
-var score = Infer( {model() {
+var humanScore = function() {
+  Infer( {model() {
   //define true values
-  var trueBeta1 = sample(RandomInteger({n:8}))+1;
-  var trueBeta2 = sample(RandomInteger({n:8}))+1;
+  var trueBeta1 = sample(TRUEBETA1);
+  var trueBeta2 = sample(TRUEBETA2);
   
   var getTrueBeta = function(state){
     var table = {
@@ -98,7 +111,50 @@ var score = Infer( {model() {
     return table[state];
   };
   
-  var trueD = sample(RandomInteger({n:9}))-4
+  var trueD = sample(TRUED);
+  var trueUtilA = 20;
+  var trueUtilB = trueUtilA + trueD;
+  
+  var trueUtility = function(state){
+    var table = {
+      A: trueUtilA,
+      B: trueUtilB
+    };
+    return table[state];
+  };
+    var state = uniformDraw(startStates);
+    var action = uniformDraw(actions);
+    var eu = expectedUtility(state, action, trueUtility);
+    var beta = getTrueBeta(state);
+    factor(eu/beta);
+    var actualUtility = trueUtility(transition(action,state));
+    var utilities = map(trueUtility, nextStates);
+    var maxUtility = reduce(max, -999999, utilities);
+    var regret = actualUtility - maxUtility;
+    //print("actual, max, regret=");
+    //print(actualUtility);
+    //print(maxUtility);
+    //print(regret);
+    return {regret};
+   }});
+};
+
+
+var score = function(length){
+  Infer( {model() {
+  //define true values
+  var trueBeta1 = sample(TRUEBETA1);
+  var trueBeta2 = sample(TRUEBETA2);
+  
+  var getTrueBeta = function(state){
+    var table = {
+      start1: trueBeta1,
+      start2: trueBeta2
+    };
+    return table[state];
+  };
+  
+  var trueD = sample(TRUED);
   var trueUtilA = 20;
   var trueUtilB = trueUtilA + trueD;
   
@@ -110,12 +166,16 @@ var score = Infer( {model() {
     return table[state];
   };
   
-  var observedTrajectory = makeTrajectory(getTrueBeta, trueUtility, 10);
-  
-  var d = sample(posterior(observedTrajectory))
-  var sameSign = d*trueD >0; //true if d and trueD have same sign
-  var reward = sameSign ? 
-      expectation(Math.abs(trueD)) 
-   : -expectation(Math.abs(trueD))
+  var observedTrajectory = makeTrajectory(getTrueBeta, trueUtility, length);
+  var d = sample(posterior(observedTrajectory)).d
+  var correctChoice = d*trueD >0; //true if d and trueD have same sign
+  var regret = correctChoice ? 0 : -Math.abs(trueD)
+  return {regret,length};
   
 }});
+};
+var humanRegret = humanScore();
+
+var robotRegrets = map(score, [1,5,10,15,20]);
+map(viz, robotRegrets);
+viz(humanRegret);
