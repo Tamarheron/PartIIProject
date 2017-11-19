ee8f714d90549488545498058e02c5eec5185f46
Elizabeth Barnes <beth.m.barnes@gmail.com>
2017-11-19 16:42:30 +0000

    robot acts based on expectation of posteerior

--- webPPL/infer.js
+++ webPPL/infer.js
@@ -24,7 +24,7 @@ var expectedUtility = function(state, action, utility) {
 
 
 var softMaxAgent = function(state, beta, utility) {
-      return Infer({ 
+      return Infer({ method:"enumerate",
         model() {
           var action = uniformDraw(actions);
           var eu = expectedUtility(state, action, utility);
@@ -58,8 +58,8 @@ var makeTrajectory = function(getBeta, utility, length) {
 };
 
 
-var posterior = dp.cache(function(observedTrajectory){
-  return Infer({model() {
+var posterior = function(observedTrajectory){
+  return Infer({method:"enumerate", model(){
   //define priors
   var beta1 = sample(TRUEBETA1);
   var beta2 = sample(TRUEBETA2);
@@ -93,13 +93,13 @@ var posterior = dp.cache(function(observedTrajectory){
       observe(softMaxAgent(state, beta, utility), action);
     },
     observedTrajectory);
-    return {d};
+  return d;
   }});
-});
+};
 
 //model the regret of the human based on their beta level
 var humanScore = function(state) {
-  Infer( {model(){
+  Infer( {method:"enumerate", model(){
   //define true values
   var trueBeta1 = sample(TRUEBETA1);
   var trueBeta2 = sample(TRUEBETA2);
@@ -141,13 +141,13 @@ var humanScore = function(state) {
     //print(actualUtility);
     //print(maxUtility);
     //print(regret);
-    return {regret, state};
+    return regret;
    }});
 };
 
 
 var score = function(length){
-  Infer( {model() {
+  Infer( { model() {
   //define true values
   var trueBeta1 = sample(TRUEBETA1);
   var trueBeta2 = sample(TRUEBETA2);
@@ -173,19 +173,30 @@ var score = function(length){
   };
   
   var observedTrajectory = makeTrajectory(getTrueBeta, trueUtility, length);
-  var d = sample(posterior(observedTrajectory)).d
+  var posteriorVar = posterior(observedTrajectory);
+  //print(posteriorVar);
+  var d = expectation(posteriorVar);
+  //print(d);
   var correctChoice = d*trueD >0; //true if d and trueD have same sign
   var regret = correctChoice ? 0 : -Math.abs(trueD)
   return {regret,length};
   
 }});
 };
-var start = Categorical({ps:[1,1], vs:startStates});
 //var observedTrajectory1 = [['start1','A'],['start1','A'],['start2','A'],['start2','A']];
 //var observedTrajectory2 = [['start1','A']];
-//viz(posterior(observedTrajectory1));
-//viz(posterior(observedTrajectory2));
-//var robotRegrets = map(score, [1,5,10,15,20]);
-//map(viz, robotRegrets);
-var humanRegret = map(humanScore, ['start1', 'start2']);
-map(viz,humanRegret);
+//var post1 = posterior(observedTrajectory1);
+//var post2 = posterior(observedTrajectory2);
+//viz(post1);
+//viz(post2);
+//print(expectation(post1));
+
+var robotRegrets = map(score, [1,2,3]);
+print(robotRegrets);
+map(viz, robotRegrets);
+
+var humanRegrets = map(humanScore, ['start1', 'start2']);
+print(humanRegrets);
+map(viz,humanRegrets);
+var humanMeans = map(expectation, humanRegrets);
+var humanRegret = listMean(humanMeans);
