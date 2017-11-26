7489025f5d56d5b211f98e6404c2b1743a8baee9
Elizabeth Barnes <beth.m.barnes@gmail.com>
2017-11-26 12:01:38 +0000

    posterior working

--- webPPL/infer.js
+++ webPPL/infer.js
@@ -1,331 +1,252 @@
-var map3 = function(fun, args1, args2, args3){
-  var fun2 = function(arg12, arg3) { return fun(arg12[0], arg12[1], arg3)}
-  var args12 =zip(args1, args2);
-  print(args12);
-  return map2(fun2, args12, args3);
-};
-
-var max = function(a,b){
-  return Math.max(a,b);
-};
-
-
-var actions = ['A', 'B'];
-var startStates = ['start1','start2'];
-var nextStates = ['A', 'B'];
-
-
-var transition = function(state, action) {
-  var nextProbs = (action === 'A') ? [1,0] : [0,1];
-  return Categorical({ps:nextProbs, vs:nextStates});
-};
-
-//distributions of true parameters and robot's belief
-var TRUED = Categorical({ps:[1,1], vs:[-1,1]});
-var TRUEBETA1 = Categorical({ps:[1,1,1], vs:[1,10]});
-var TRUEBETA2 = Categorical({ps:[1,1,1], vs:[1,10]});
-
-var TRUEUTILA = 20;
-
-
-var getTrueBetaDist = function(state){
-    var table = {
-      start1: TRUEBETA1,
-      start2: TRUEBETA2
-    };
-    return table[state];
-};
-
-
-  var getTrueUtility = function(state, d){
-    var table = {
-      A: TRUEUTILA,
-      B: TRUEUTILA + d
-    };
-    return table[state];
+var createMDP = function(options){
+  // takes parameters: states, startStates, actions, transitions, utilities, betas
+  assert.ok(_.has(options, 'states') &&
+            _.has(options, 'startStates') &&
+            _.has(options, 'actions') &&
+            _.has(options, 'transitions') &&
+            _.has(options, 'utilities') &&
+            _.has(options, 'betas'),
+      'createMDP args: options does not contain one or more' +
+            ' of states, startStates, actions, transitions, utilities, betas');
+  var states = options.states;
+  
+  var getBeta = function(state){
+    var index = states.indexOf(state);
+    assert.ok((index!=-1), 'state not found');
+    return options.betas[index];
   };
-
-var getUtilities = function(d){
- var dFun = function(n){return d;};
- // create an array of length(nextStates) ds
- var n = nextStates.length;
- var ds = mapN(dFun, n);
- return map2(getTrueUtility, nextStates, ds);
-};
-
-var expectedUtility = function(state, action, d) {
-       return expectation(Infer({method:"enumerate", model() { 
-         var current = state;
-         //print(transition(current, action));
-         var next = sample(transition(current, action));
-         var util = getTrueUtility(next,d);
+  
+  var getActions = function(state){
+    var index = options.states.indexOf(state);
+    assert.ok((index!=-1), 'state not found');
+    return options.actions[index];
+  };
+  
+  var getUtility = function(state){
+    var index = options.states.indexOf(state);
+    assert.ok((index!=-1), 'state not found');
+    return options.utilities[index];
+  };
+  
+  var getTransition = options.transitions;
+  var startStates = options.startStates;
+  var length = options.length;
+  
+  var getExpectedUtility = function(options) {
+       assert.ok(_.has(options, 'state') &&
+            _.has(options, 'action'), 
+                 'getExpectedUtility args:' +
+                 'one or more of state, action is missing');
+    var expected =  expectation(Infer({model() { 
+         var current = options.state;
+         var action = options.action;
+         var next = sample(getTransition({state:current, action:action}));
+         var util = getUtility(next);
          return util;
-       }}));
+
+       }}));     
+    return expected;
+    
+
+  };
+  
+  return{getBeta, getActions, getUtility, getExpectedUtility,
+         getTransition, startStates, states};
 };
 
+  
 
-var softMaxAgent = function(state, beta, d) {
-      return Infer({ method:"enumerate",
+
+var softMaxAgent = function(options) {
+  assert.ok( _.has(options, 'MDP') &&
+            _.has(options, 'discount'),
+                 'softMaxAgent args:' +
+                 'one or more of discount, MDP is missing');
+  var MDP = options.MDP;
+  var getActions = MDP.getActions;
+  var getBeta = MDP.getBeta;
+  var discount = options.discount;
+  var getTransition=MDP.getTransition;
+  var getUtility = MDP.getUtility;
+  
+  var getEU = function(options) {
+    //console.log('called eu');
+       assert.ok(_.has(options, 'state') &&
+                 _.has(options, 'action') &&
+                 _.has(options, 'length'), 
+                 'getExpectedUtility args:' +
+                 'one or more of state, action is missing');
+    var expected =  expectation(Infer({method:'enumerate', model() { 
+       var current = options.state;
+       var action = options.action;
+       var next = sample(getTransition({state:current, action:action}));
+       var util = getUtility(next);
+       if (length==0){
+         return util;
+       } else{
+         var nextAction = act({state:next, length:length});
+         var newLength = length-1;
+         var futureUtil = discount*getEU({
+           state:next, action:nextAction, length:newLength})
+         return (util + futureUtil);
+
+       }}}));  
+    return expected;
+  };
+  
+  var act = function(options){
+    //console.log('called act');
+    assert.ok(_.has(options, 'state') &&
+              _.has(options, 'length'), 
+                'act args: missing one or more of: state, length');
+    var state=options.state;
+    var length=options.length;
+    var beta = getBeta(state);
+    var actions = getActions(state);
+    return Infer({ method:"enumerate",
         model() {
           var action = uniformDraw(actions);
-          var eu = expectedUtility(state, action, d);
-          var debugPrint = !(beta*d*eu<9999999);
+          var eu = getEU({state: state, action:action, length:length});
+          var debugPrint = false;
           if (debugPrint){
-            print("action, state, beta, d, eu =");
-            print(action);
-            print(state);
-            print(beta);
-            print(d);
-            print(eu);
-            print("factor");
-            print(eu/beta);
+            console.log("action: " + action + " state: " + state + 
+                 " beta: " + beta + " eu: "+eu+" factor: "+eu/beta );
           }
-          
           factor(eu/beta);
           
         return action;
         }
-      });
-
+   });
+  };
+  return {getEU, act};
 };
 
 //generate sample trajectory
-var makeTrajectory = function(length, d, getBeta) {
-  var step = function(){
-    var state = uniformDraw(startStates);
-    var beta = getBeta(state);
-    var action = sample(softMaxAgent(state, beta, d));
-    return [state, action];
+var makeTrajectory = function(options) {
+  //console.log('calling maketraj');
+   assert.ok(_.has(options, 'length') &&
+             _.has(options, 'agent') &&
+            _.has(options, 'MDP'), 
+                 'makeTrajectory args:' +
+                 'one or more of length, agent, MDP is missing');
+  var MDP = options.MDP;
+  var length = options.length;
+  var start = uniformDraw(MDP.startStates);
+  //console.log('start is ' + start + ' length is '+ length);
+  var agent = options.agent;
+  var act=agent.act;
+  var transition=MDP.getTransition;
+  
+  var step = function(options){
+    console.log('calling step');
+    assert.ok(_.has(options, 'state') &&
+              _.has(options, 'length'), 
+                 'makeTrajectory.step args:' +
+                 'one or more of length, state is missing');
+    var state=options.state;
+    var length=options.length;
+    var action = sample(act({state:state, length:length}));
+    var transDist = transition({state:state, action:action})
+    var next = sample(transDist);
+    var res = [state, action];
+    var newLength = length-1;
+    var debug=false;
+    if (debug) {console.log('action: ' + action + ' trnsDist: '+transDist+
+               ' length: '+length +' newLen: '+newLength+ ' next: '+ next);};
+    return length==1 ? 
+    [res] : 
+    [res].concat(step({state:next, length: newLength}));
   };
-  var res = step()
-  return length==1 ? [res] : [res].concat(makeTrajectory(length-1, d, getBeta));
+  
+  return step({state:start, length:length});
+  
 };
 
 
-var posterior = function(observedTrajectory){
-  return Infer({model(){
+var posterior = function(options){
+  assert.ok(_.has(options, 'priors') &&
+            _.has(options, 'MDPParams') &&
+            _.has(options, 'discount') &&
+           _.has(options, 'observedTrajectory'),
+           'posterior args missing one or more of ' +
+            'priors, MDPParams,discount, observedTrajectory');
+  
+  var priors = options.priors;
+  var traj = options.observedTrajectory;
+  var params = options.MDPParams;
+  var discount=options.discount;
+  
+  var trajLength = traj.length;
+  var remainingLengths = mapN(
+      function(n){return trajLength-n}, trajLength)
+  
+  var debug =false;
+  
+  return Infer({method:"enumerate",model(){
     //sample from priors
-    var d = sample(TRUED);
-    var beta1 = sample(TRUEBETA1);
-    var beta2 = sample(TRUEBETA2);
-    var getBeta = function(state){
-      var table = {
-        start1: beta1,
-        start2: beta2
-      };
-    return table[state];
-    };
+    var utilities = map(sample,priors.utilities);
+    var betas = map(sample,priors.betas);
+    
+    if (debug) {console.log('sample utilities '+utilities+', betas: '+betas)}
+    
+    //create MDP based on known params and sampled utilities and betas
+    var MDP = createMDP({actions:params.actions, transitions:params.transitions, 
+                     states:params.states, startStates:params.startStates, 
+                     utilities:utilities, betas:betas});
+    var agent = softMaxAgent({MDP:MDP, discount:discount});
+    var act = agent.act;
 
-    // For each observed state-action pair, factor on likelihood of action
-    map(
-      function(stateAction){
+    
+    // For each observed state-action pair, factor on likelihood of action,
+    //given the remaining length
+    map2(
+      function(stateAction, length){
         var state = stateAction[0];
-        var beta = getBeta(state);
         var action = stateAction[1];
-        observe(softMaxAgent(state, beta, d), action);
+        observe(act({state:state,length:length }), action);
       },
-      observedTrajectory);
-    return d;
+      traj, remainingLengths);
+    return {utilities:utilities, betas:betas};
   }});
 };
 
-//model the regret of the human based on their beta level
-var humanScore = function(state, d, beta) {  
-  Infer( { model(){
-    var action = sample(softMaxAgent(state, beta, d));
-    var nextState = sample(transition(state, action));
-    var actualUtility = getTrueUtility(nextState, d);
-    var utilities = getUtilities(d);
-    var maxUtility = reduce(max, -999999, utilities);
-    var regret = actualUtility - maxUtility;
-    var debug = false;
-    if (debug) {
-      print("action, nextState, actual, max, regret=");
-      print(action);
-      print(nextState);
-      print(actualUtility);
-      print(maxUtility);
-      print(regret);
-    };
-    return regret;
-   }});
+var actions = [['A', 'B'],['A', 'B'], ['A', 'B'], ['A', 'B']];
+var transition = function(options) {
+  assert.ok( _.has(options, 'state') && 
+             _.has(options, 'action'), 'transition args: missing one or more ' +
+           'of state, action');
+  var action=options.action;
+  var state=options.state;
+  var nextProbs = (action === 'A') ? [0,0,1,0] : [0,0,0,1];
+  return Categorical({ps:nextProbs, vs:states});
 };
 
+var states = ['start1', 'start2', 'A', 'B'];
+var startStates = ['start1','start2'];
+var betas = [1,1,1,10];
+var utilities = [1,1,1,20];
 
-var robotScore = function(length, d, getBeta){
-  Infer({model() {   
-    var observedTrajectory = makeTrajectory(length, d, getBeta);
-    var posteriorD = posterior(observedTrajectory);  
-    var estimateD = expectation(posteriorD);
-    var debug = false;
-    if (Math.abs(estimateD) < 0.000001){ // robot chooses randomly
-       var regret = -Math.abs(d)*0.5;
-       if (debug) {
-        print("traj, posterior, estimateD, regret")
-        print(observedTrajectory)
-        print(posteriorD);
-        print(estimateD);
-        print(regret);    
-      }
-       return regret;
-      }
-    else{
-      var correctChoice = (d*estimateD)>0; //true if d and estimateD have same sign
-      var regret = correctChoice ? 0 : -Math.abs(d);
-      
-      if (debug) {
-        print("traj, posterior, correct, estimateD, regret")
-        print(observedTrajectory)
-        print(posteriorD);
-        print(correctChoice);
-        print(estimateD);
-        print(regret);    
-      };
-      return regret;
-  }}});
-};
+var params = {actions:actions, transitions:transition, states:states, 
+              startStates:startStates};
 
+var utilityPriors = mapN(
+  function(n){return Categorical({ps:[2,1], vs:[1,20]})}
+  , 4);
+var betaPriors = mapN(
+  function(n){Categorical({ps:[1,1], vs:[1,10]})}, 
+  4);
+var priors = {utilities:utilityPriors, betas:betaPriors};
 
-  //returns counts of how many times each action appears in a trajectory
- var getCounts = function(traj) {
-   var count = function(target){
-      var test = function(stateActionPair) {
-        return (target == stateActionPair[1]) ? 1 : 0
-      }
-      var flags = map(test, traj); //array containing a 1 for each match
-      return sum(flags);
-    }
-    var counts = map(count, actions);
-    //print(counts);
-    return counts;  
- };
-  
-  
+console.log(utilityPriors, betaPriors);
 
-var naiveRobotScore = function(length, d, getBeta){
-  //Naive robot chooses the most frequent action from the observed traj
-  Infer({model() {  
-    var observedTrajectory = makeTrajectory(length, d, getBeta);
-    var counts = getCounts(observedTrajectory);
+var MDP = createMDP({states:states,startStates:startStates, actions:actions, 
+                    transitions:transition, utilities:utilities, betas:betas});
 
-    var frequencyDifference = counts[1]-counts[0];
-    if (frequencyDifference==0) {
-      //in this case, the two actions have equal frequencies 
-      //and robot chooses at random
-      var regret = -0.5*Math.abs(d);
-      //print(regret);
-      return regret;
-    } 
-    else{ //robot chooses most frequent action
-      var correctChoice = d*frequencyDifference >0; 
-      //true (robot chooses correctly) if d and frequencyDifference have same sign
-      var regret = correctChoice ? 0 : -Math.abs(d)
-      //print(regret);
-      return regret;
-    }
+var agent = softMaxAgent({discount:0.9, MDP:MDP});
+var traj = makeTrajectory({length:10, agent:agent, MDP:MDP});
 
-  }});
-};
+print(traj);
 
-var sampleBeta1 = 1;
-var sampleBeta2 = 1;
-
-var getSampleBeta = function(state){
-    var table = {
-      start1: sampleBeta1,
-      start2: sampleBeta2
-    };
-    return table[state];
-};
-
-var simulateRobot = function (scorer, lengths, ds, betas) {
-  var getBeta = function(state){
-      var table = {
-        start1: betas[0],
-        start2: betas[1]
-      };
-      return table[state];
-    };
-  
-  return Infer({method:"enumerate", model(){
-    var length = uniformDraw(lengths);
-    var d = uniformDraw(ds);
-    var expectedScore = expectation(scorer(length, d, getBeta));
-    var debug = true;
-    if (debug) {
-      print("length, d,expectedScore =");
-      print(length);
-      print(d);
-      print(expectedScore);
-    }
-    return expectedScore;
-  }});
-};
-
-var simulateHuman = function (scorer, states, ds, betas) {
-    var dists = map3()
-    var state = uniformDraw(states);
-    var beta = getBeta(state);
-    var d = uniformDraw(ds);
-    var expectedScore = expectation(scorer(state, d, beta));
-    var debug = false;
-    if (debug) {
-      print("state, d, beta, expectedScore =");
-      print(state);
-      print(d);
-      print(beta);
-      print(expectedScore);
-    }
-    return expectedScore;
-  }});
-};
-
-
-var humanRegret = humanScore('start1', 1, 1.5);
-print(humanRegret);
-viz(humanRegret);
-
-
-//var scores = simulateHuman(humanScore, startStates, [1,2,3], [TRUEBETA1, TRUEBETA2]);
-//print(scores);
-//viz(scores);
-
-var betas = [[1,1],[2,2],[3,3],[4,4], [5,5], [6,6], [7,7]];
-
-var humanScoresBetas = function(betas){
-  return map(function(pair) { return expectation(simulateHuman(
-    humanScore, 
-    startStates, 
-    [1],
-    pair));
-                            },
-    betas
-            );
-};
-
-var robotScoresLengths = function(lengths, type){
-  return map(function(length) { return expectation(simulateRobot(
-    type, 
-    [length], 
-    [1],
-    [1,10]));
-                            },
-    lengths
-            );
-};
-
-var getBeta = function(state){
-      var table = {
-        start1: 1,
-        start2: 10
-      };
-      return table[state];
-    };
-var lengths = [10];
-//var scores1 = robotScoresLengths(lengths, robotScore);
-//var scores2 = robotScoresLengths(lengths, naiveRobotScore);
-var scores3 = expectation(robotScore(10, 1, getBeta));
-//print(scores1);
-print(scores3);
-//viz.bar(lengths, scores1);
-viz.bar(lengths, scores3); 
+var post = posterior({priors:priors, MDPParams:params, 
+                      discount:0.9, observedTrajectory:traj});
+viz(post);
